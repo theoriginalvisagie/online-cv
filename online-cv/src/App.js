1dd6895f5ade3026d2f73713a17255856d788fd9
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-01-18 19:04:30 +0200

    Headings component added

--- online-cv/src/App.js
+++ online-cv/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Headings from './components/Headings';
 
 function App() {
   return (
     <div className="App">
-      
+      <Headings text='Full Stack Developer | Labmin' start='2020 - ' end='Current'/>
+      <Headings text='Photographer | Model Co Cape Town'/>
     </div>
   );
 }
